6a5a518f8842155131d2fcfbf829fa33b99d864c
Bohdan Konovalenko <60445869+Fagtoy@users.noreply.github.com>
2023-04-21 11:14:43 +0300

    fix: no need to push yc-documentation container (#189)

--- kubescripts/k8s-push-images.sh
+++ kubescripts/k8s-push-images.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for image in yc_redis yc_module-compilation yc_admin-ui yc_frontend yangvalidator yc_yangre yc_yangcatalog-ui catalog_backend_api yc_confd yc_documentation; do
+for image in yc_redis yc_module-compilation yc_admin-ui yc_frontend yangvalidator yc_yangre yc_yangcatalog-ui catalog_backend_api yc_confd; do
   echo $image
   docker tag $image:latest localhost:32000/$image:latest
   docker push localhost:32000/$image:latest
